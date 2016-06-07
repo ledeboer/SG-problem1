54ccd1401e833f2c32a8c338e1da1ab5185a3f30
Paul Ledeboer <paul.ledeboer@gmail.com>
2016-06-07 18:21:31 -0500

    Initial Commit

---
+++ SG-problem1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SG-problem1
+//
+//  Created by Paul Ledeboer on 6/7/16.
+//  Copyright © 2016 Paul Ledeboer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
